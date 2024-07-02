cf1aff57ac80b7da7ae53aece07fcecc6deb8522
Anirudh Sriram <srirama@cs.uchicago.edu>
2024-07-02 21:24:49 +0200

    Cart updates

--- src/app/_libs/authOptions.ts
+++ src/app/_libs/authOptions.ts
@@ -1,13 +1,2 @@
 import GoogleProvider from "next-auth/providers/google";
 import NextAuth from "next-auth";
-
-// export const authOptions = {
-//   //   secret: process.env.MY_SECRET,
-//   providers: [
-//     GoogleProvider({
-
-//       //   clientId: process.env.GOOGLE_CLIENT_ID as string,
-//       //   clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-//     }),
-//   ],
-// };
