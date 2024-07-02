b4a21d7af9583b4919b4b4013eaf32b0028fe19d
Anirudh Sriram <srirama@cs.uchicago.edu>
2024-07-02 21:23:20 +0200

    Cart updates

--- src/app/_libs/authOptions.ts
+++ src/app/_libs/authOptions.ts
@@ -3,12 +3,9 @@ import NextAuth from "next-auth";
 
 // export const authOptions = {
 //   //   secret: process.env.MY_SECRET,
-//   secret: "hellothere",
 //   providers: [
 //     GoogleProvider({
-//     //   clientId:
-//     //     "882701301507-cd9sliq6ffl4e0n34tl14dhosi6vp6ra.apps.googleusercontent.com",
-//     //   clientSecret: "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",
+
 //       //   clientId: process.env.GOOGLE_CLIENT_ID as string,
 //       //   clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
 //     }),
